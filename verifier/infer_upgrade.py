55fd3570a80148d7fa5f70adf5e7ce201141d77a
sallywang147 <sallyjunsongwang@gmail.com>
2024-05-13 04:35:09 +0000

    add light functions

--- verifier/infer_upgrade.py
+++ verifier/infer_upgrade.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import openai
 from openai import OpenAI
+from prompts.bug_search import search
 #uncomment and use the following line instead when calling from smartinv.py
 #from .prompts.GPTScan.query_template import prompt_single_yes_no_question, prompt_multiple_choice_scenarios 
 from prompts.GPTScan.query_template import prompt_single_yes_no_question, prompt_multiple_choice_scenarios 
@@ -106,8 +107,105 @@ def find_invariants(folder, file_results_dir, limit):
 		print(raw_output)
 		result_file.write(raw_output)
 		result_file.close()   
-	    
 
+def interpret_output(folder, file_results_dir):
+	'''
+	for root, dirs, files in os.walk(folder):
+		for filename in files:
+			contract_file = os.path.join(root, filename)
+			print(contract_file)
+			result_file_path = os.path.join(file_results_dir, f"{filename}_bug.txt")
+	'''
+	bug = ""
+	result_file_path = "./test/numbered_timecontroller.sol_bug.txt"
+	if os.path.isfile(result_file_path):
+		result_file = open(f'{result_file_path}', "r")
+		for i in result_file.readlines():
+			i = i.split(":")
+			if "1" in i[0] and "Yes" in i[1]:
+				bug += "Reentrancy, "
+				print("Potential reentrancy identified")
+			if "2" in i[0] and "Yes" in i[1]:
+				bug += "Integer overflow/underflow, "
+				print("Potential integer overflow/underflow identified")
+			if "3" in i[0] and "Yes" in i[1]:
+				bug += "Arithmetic flaw, "
+				print("Potential arithmetic flaw identified")
+			if "4" in i[0] and "Yes" in i[1]:
+				bug += "Suicidal contract, "
+				print("Potential suicidal contract identified")
+			if "5" in i[0] and "Yes" in i[1]:
+				bug += "Ether leakage, "
+				print("Potential ether leaakage identified")
+			if "6" in i[0] and "Yes" in i[1]:
+				bug += "Insufficient gas, "
+				print("Potential insufficient gas identified")
+			if "7" in i[0] and "Yes" in i[1]:
+				bug += "Incorrect ownership/visibility, "
+				print("Potential incorrect ownership/visibility identified")
+			if "8" in i[0] and "Yes" in i[1]:
+				bug += "Price manipulation, "
+				print("Potential price manipulation identified")
+			if "9" in i[0] and "Yes" in i[1]:
+				bug += "Priviledge escalation, "
+				print("Potential priviledge escalation identified")
+			if "10" in i[0] and "Yes" in i[1]:
+				bug += "Atomicity violation, "
+				print("Potential atomicity violation identified")
+			if "11" in i[0] and "Yes" in i[1]:
+				bug += "Business logic flaw, "
+				print("Potential business logic flaw identified")
+			if "12" in i[0] and "Yes" in i[1]:
+				bug += "Inconsistent state update, "
+				print("Potential inconsistent state update identified")
+			if "13" in i[0] and "Yes" in i[1]:
+				bug += "Cross bridge inconsistency, "
+				print("Potential cross bridge inconsistency identified")
+			if "14" in i[0] and "Yes" in i[1]:
+				bug += "ID Uniqueness violation, "
+				print("Potential ID Uniqueness violation identified")
+			if "15" in i[0] and "Yes" in i[1]:
+				if bug =="": 
+					bug += "Healthy!"
+					print("SmartInv Light thinks the contract might be healthy, but can be wrong. Please verify.")
+				else: 
+            				continue
+	return bug
+		
+		
+def find_bugs_light_mode(folder, file_results_dir, limit):
+	if (os.path.isdir(file_results_dir)) is False:
+		os.mkdir(file_results_dir)
+	'''
+	for root, dirs, files in os.walk(folder):
+		for filename in files:
+			contract_file = os.path.join(root, filename)
+			print(contract_file)
+	'''
+	contract_file = "./numbered_timecontroller.sol"
+	filename = "numbered_timecontroller.sol"
+	preparation_1 = open(contract_file, "r")
+	prior_inv = os.path.join(file_results_dir, f"{filename}_inv.txt")
+	if os.path.isfile(prior_inv): 
+		preparation_2 = open(prior_inv, "r")
+	x = len(preparation_1.readlines() + preparation_2.readlines())
+	result_file_path = os.path.join(file_results_dir, f"{filename}_bug.txt")
+	if os.path.isfile(contract_file) and x != 1 and x <=limit:			
+		result_file = open(f'{result_file_path}', "w+")
+		if os.path.isfile(prior_inv): 
+			preparation_2 = open(prior_inv, "r")
+		preparation_1 = open(contract_file, "r")
+		prompt = preparation_1.read() + preparation_2.read()
+		full_prompt = search(prompt)
+		raw_output = gpt_infer(full_prompt)
+		print(f"=========bugs ID inferred for {filename}=======")
+		print(raw_output)
+		bug = interpret_output(folder, file_results_dir)
+		print(f"=========After translation, bugs inferred for {filename}=======")
+		print(bug)
+		result_file.write(bug)
+		result_file.close()   
+  
 def infer_bugs(folder, file_results_dir, exp_name, prompt, limit):
 	list = []
 	path =f"/home/sallyjunsongwang/SmartInv/verifier/prompts/{prompt}"
@@ -177,8 +275,10 @@ if __name__ == "__main__":
 	manual_prompt = "manual_audit.txt"
 	limit = 300
 	audited_bug_folder_path = f"../tests/refined_analysis/natural_bugs/additional_audited_bugs"
-	find_program_points(audited_bug_folder_path, "./test", limit)
-	find_invariants(audited_bug_folder_path, "./test", limit)
+	#find_program_points(audited_bug_folder_path, "./test", limit)
+	#find_invariants(audited_bug_folder_path, "./test", limit)
+	#find_bugs_light_mode(audited_bug_folder_path, "./test", limit)
+	interpret_output(audited_bug_folder_path, "./test")
 	'''
 	infer_bugs(audited_bug_folder_path, file_results_dir, exp_name_manual, manual_prompt, limit)
 	for i in sets:
